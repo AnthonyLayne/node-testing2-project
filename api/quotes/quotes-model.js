d3e60eb238760e7b9c52424154943b32e83f01b0
AnthonyLayne <kepheus.ds@gmail.com>
2022-05-26 20:18:49 -0400

    getById model

--- api/quotes/quotes-model.js
+++ api/quotes/quotes-model.js
@@ -4,6 +4,18 @@ function getAll() {
   return db("quotes");
 }
 
+function getById(quote_id) {
+  return db("quotes").where("quote_id", quote_id).first();
+}
+
+async function insert(quote) {
+  return db("quotes")
+    .insert(quote)
+    .then(([quote_id]) => getById(quote_id));
+}
+
 module.exports = {
   getAll,
+  getById,
+  insert,
 };
